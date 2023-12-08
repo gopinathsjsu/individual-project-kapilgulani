f90eb62586681532695469b83cd396a23944ea0d
kapilgulani <144957546+kapilgulani@users.noreply.github.com>
2023-12-08 00:21:26 -0800

    xml class running correctly

--- XMLAdapter.java
+++ XMLAdapter.java
@@ -31,8 +31,17 @@ public class XMLAdapter implements Adapter {
             String expirationDate = cardElement.getElementsByTagName("EXPIRATION_DATE").item(0).getTextContent();
             String cardHolderName = cardElement.getElementsByTagName("CARD_HOLDER_NAME").item(0).getTextContent();
 
-            CreditCard card = CreditCardFactory.getCreditCard(cardNumber, expirationDate, cardHolderName);
-            if (card != null && card.isValid()) {
+            // Determine the card type and handle invalid card numbers
+            String errorMessage = determineErrorMessage(cardNumber);
+            CreditCard card;
+            if (!errorMessage.isEmpty()) {
+                card = new InvalidCreditCard(cardNumber, expirationDate, cardHolderName, errorMessage);
+            } else {
+                card = CreditCardFactory.getCreditCard(cardNumber, expirationDate, cardHolderName);
+            }
+            
+            // Add this null check
+            if (card != null) {
                 cards.add(card);
             }
         }
@@ -40,42 +49,87 @@ public class XMLAdapter implements Adapter {
         return cards;
     }
 
+    private String determineErrorMessage(String cardNumber) {
+        if (cardNumber == null || cardNumber.isEmpty()) {
+            return "Invalid: empty/null card number";
+        }
+        if (cardNumber.length() > 19) {
+            return "Invalid: more than 19 digits";
+        }
+        if (!cardNumber.matches("\\d+")) {
+            return "Invalid: non numeric characters";
+        }
+        if (!isPossibleCardNumber(cardNumber)) {
+            return "Invalid: not a possible card number";
+        }
+        return "";
+    }
+
+    private boolean isPossibleCardNumber(String cardNumber) {
+        // Example logic: Check if the card number starts with certain digits
+        // and has a valid length. Adjust this logic according to your specific rules.
+        if (cardNumber.startsWith("4") && (cardNumber.length() == 13 || cardNumber.length() == 16)) {
+            return true; // Visa
+        }
+        if (cardNumber.startsWith("5") && cardNumber.length() == 16) {
+            return true; // MasterCard
+        }
+        if ((cardNumber.startsWith("34") || cardNumber.startsWith("37")) && cardNumber.length() == 15) {
+            return true; // American Express
+        }
+        if (cardNumber.startsWith("6011") && cardNumber.length() == 16) {
+            return true; // Discover
+        }
+        return false; // Not a plausible card number
+    }
+
     @Override
     public void writeCardsToFile(List<CreditCard> cards, String filename) throws Exception {
         // Create a DocumentBuilder
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-
+    
         // Create a new XML document
         Document document = builder.newDocument();
         Element rootElement = document.createElement("CARDS");
         document.appendChild(rootElement);
-
+    
         for (CreditCard card : cards) {
             Element cardElement = document.createElement("CARD");
-
+    
             Element cardNumberElement = document.createElement("CARD_NUMBER");
             cardNumberElement.setTextContent(card.getCardNumber());
             cardElement.appendChild(cardNumberElement);
-
+    
             Element cardTypeElement = document.createElement("CARD_TYPE");
-            cardTypeElement.setTextContent(card instanceof InvalidCreditCard ?
-                    ((InvalidCreditCard) card).getErrorMessage() :
-                    card.getClass().getSimpleName().replace("CC", ""));
+            if (card instanceof InvalidCreditCard) {
+                cardTypeElement.setTextContent(((InvalidCreditCard) card).getErrorMessage());
+            } else {
+                // Set the card type based on the class name or a specific method
+                cardTypeElement.setTextContent(getCardType(card));
+            }
             cardElement.appendChild(cardTypeElement);
-
+    
             rootElement.appendChild(cardElement);
         }
-
+    
         // Configure formatting for proper indentation
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty("indent", "yes");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
+    
         // Write the XML document to a file
         DOMSource source = new DOMSource(document);
         StreamResult result = new StreamResult(new File(filename));
         transformer.transform(source, result);
     }
+    
+    private String getCardType(CreditCard card) {
+        // Implement the logic to determine the card type based on the card instance.
+        // This could be as simple as returning the class name, or more complex logic based on card properties.
+        // Example:
+        return card.getClass().getSimpleName().replace("CC", "");
+    }
+    
 }
