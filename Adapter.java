d5e63bb75718abdc8ccf5156729c33b2716f161c
kapilgulani <144957546+kapilgulani@users.noreply.github.com>
2023-12-10 15:38:17 -0800

    changed the file name and code from Adapter to Stratagy

--- Adapter.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface Adapter {
-    List<CreditCard> readCardsFromFile(String filename) throws Exception;
-    void writeCardsToFile(List<CreditCard> cards, String filename) throws Exception;
-}
